d6d97ca954a523e8c939c7314219387b5b43fd6d
ASTRELION <5625400-ASTRELION@users.noreply.gitlab.com>
2023-12-03 20:24:53 +0000

    Add day 2 solution

---
+++ 2023/src/day2/mod.rs
@@ -0,0 +1,102 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub fn part1() -> Result<i32, Box<dyn Error>>
+{
+    let mut file = File::open("src/day2/input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let possible_cubes = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14)
+    ]);
+    let mut possible_games = 0;
+
+    for line in contents.lines()
+    {
+        let game_split: Vec<&str> = line.split(": ").collect();
+        let game_id = game_split[0]
+            .split(" ")
+            .collect::<Vec<&str>>()[1]
+            .parse::<i32>()
+            .unwrap();
+        let mut valid_game = true;
+
+        let sets: Vec<&str> = game_split[1].split("; ").collect();
+        for set in sets
+        {
+            let cubes: Vec<&str> = set.split(", ").collect();
+            for cube in cubes
+            {
+                let cube_split: Vec<&str> = cube.split(" ").collect();
+                let count: i32 = cube_split[0].parse().unwrap();
+                let color = cube_split[1];
+                if let Some(&value) = possible_cubes.get(color)
+                {
+                    if value < count
+                    {
+                        valid_game = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if valid_game
+        {
+            possible_games += game_id;
+        }
+    }
+
+    println!("Day 2, Part 1 Solution: {}", possible_games);
+    return Ok(possible_games);
+}
+
+pub fn part2() -> Result<i32, Box<dyn Error>>
+{
+    let mut file = File::open("src/day2/input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut power_sum = 0;
+
+    for line in contents.lines()
+    {
+        let game_split: Vec<&str> = line.split(": ").collect();
+        let game_id = game_split[0]
+            .split(" ")
+            .collect::<Vec<&str>>()[1]
+            .parse::<i32>()
+            .unwrap();
+        let mut min_cubes = HashMap::from([
+            ("red", 1),
+            ("green", 1),
+            ("blue", 1)
+        ]);
+
+        let sets: Vec<&str> = game_split[1].split("; ").collect();
+        for set in sets
+        {
+            let cubes: Vec<&str> = set.split(", ").collect();
+            for cube in cubes
+            {
+                let cube_split: Vec<&str> = cube.split(" ").collect();
+                let count: i32 = cube_split[0].parse().unwrap();
+                let color = cube_split[1];
+                min_cubes.insert(color, std::cmp::max(
+                    min_cubes.get(color).unwrap().to_owned(),
+                    count
+                ));
+            }
+        }
+
+        power_sum += min_cubes.values().fold(1, |acc, elem| acc * elem);
+    }
+
+    println!("Day 2, Part 2 Solution: {}", power_sum);
+    return Ok(power_sum);
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod day2;
 
 fn main()
 {
@@ -6,4 +7,7 @@ fn main()
 
     day1::part1();
     day1::part2();
+
+    day2::part1();
+    day2::part2();
 }
