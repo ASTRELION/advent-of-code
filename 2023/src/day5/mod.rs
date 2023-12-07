2a23fe79c302f765b5be273ef7e92c3c8fca7b33
ASTRELION <5625400-ASTRELION@users.noreply.gitlab.com>
2023-12-07 00:31:17 +0000

    Improve day 5 part 2 solution

--- 2023/src/day5/mod.rs
+++ 2023/src/day5/mod.rs
@@ -16,7 +16,7 @@ pub fn part1() -> Result<i64, Box<dyn Error>>
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let lines: Vec<&str> = contents.lines().collect();
+    let lines: Vec<_> = contents.lines().collect();
 
     let mut maps: Vec<Vec<Vec<i64>>> = Vec::new();
     let mut m: i64 = -1;
@@ -29,7 +29,7 @@ pub fn part1() -> Result<i64, Box<dyn Error>>
             continue;
         }
 
-        let chars: Vec<char> = line.chars().collect();
+        let chars: Vec<_> = line.chars().collect();
         if chars[0].is_digit(10)
         {
             maps[m as usize].push(split_numbers(line.to_string()));
@@ -74,7 +74,8 @@ pub fn part2() -> Result<i64, Box<dyn Error>>
 
     let lines: Vec<&str> = contents.lines().collect();
 
-    let mut maps: Vec<Vec<Vec<i64>>> = Vec::new();
+    // Maps of ranges in the form ((src_start, src_end), (dest_start, dest_end))
+    let mut maps: Vec<Vec<((i64, i64), (i64, i64))>> = Vec::new();
     let mut m: i64 = -1;
 
     for l in 2..lines.len()
@@ -88,7 +89,12 @@ pub fn part2() -> Result<i64, Box<dyn Error>>
         let chars: Vec<char> = line.chars().collect();
         if chars[0].is_digit(10)
         {
-            maps[m as usize].push(split_numbers(line.to_string()));
+            let split = split_numbers(line.to_string());
+            // ingest ranges as ((src_start, src_end), (dest_start, dest_end))
+            maps[m as usize].push((
+                (split[1], split[1] + split[2] - 1),
+                (split[0], split[0] + split[2] - 1)
+            ));
         }
         else
         {
@@ -97,36 +103,65 @@ pub fn part2() -> Result<i64, Box<dyn Error>>
         }
     }
 
-    // src len
-    let seed_ranges = split_numbers(lines[0].replace("seeds: ", ""));
+    let split = split_numbers(lines[0].replace("seeds: ", ""));
+    // (start, end)
+    let mut seed_ranges: Vec<_> = split.chunks(2).map(|c| (c[0], c[0] + c[1] - 1)).collect();
     let mut min_location = std::i64::MAX;
 
-    // this only took my computer 92 minutes to run 😀
-    // TODO: do it the actual way I should be doing this (mapping intervals to intervals)
-    for s in (0..seed_ranges.len()).step_by(2)
+    for map in maps
     {
-        for seed in seed_ranges[s]..(seed_ranges[s] + seed_ranges[s + 1] - 1)
+        let mut new_range: Vec<(i64, i64)> = Vec::new();
+        for range in &seed_ranges
         {
-            let mut source = seed;
-            for map in &maps
-            {
-                // dest src len
-                for range in map
-                {
-                    if source >= range[1] && source < range[1] + range[2]
-                    {
-                        source = range[0] + source - range[1];
-                        break;
-                    }
-                }
-            }
-
-            min_location = std::cmp::min(min_location, source);
+            new_range.extend(&map_range(&map, range));
         }
+        seed_ranges = new_range;
+    }
+
+    for range in seed_ranges
+    {
+        min_location = std::cmp::min(min_location, range.0);
     }
 
     println!("Day 5, Part 2 Solution: {}", min_location);
     return Ok(min_location);
+}
+
+fn map_range(map: &Vec<((i64, i64), (i64, i64))>, range: &(i64, i64)) -> Vec<(i64, i64)>
+{
+    let mut destinations: Vec<(i64, i64)> = Vec::new();
+
+    for (src, dest) in map
+    {
+        // [ { } ]
+        if range.0 >= src.0 && range.1 <= src.1
+        {
+            destinations.push((dest.0 + range.0 - src.0, dest.1 + range.1 - src.1));
+        }
+        // [ { ] }
+        else if range.0 >= src.0 && range.0 <= src.1
+        {
+            let inner_range = (dest.0 + range.0 - src.0, dest.1);
+            destinations.push(inner_range);
+            destinations.extend(map_range(map, &(src.1 + 1, range.1)));
+        }
+        // { [ } ]
+        else if range.1 <= src.1 && range.1 >= src.0
+        {
+            let inner_range = (dest.0, dest.0 + range.1 - src.0);
+            destinations.push(inner_range);
+            destinations.extend(map_range(map, &(range.0, src.0 - 1)));
+        }
+        // { } [ ]
+        else {}
+    }
+
+    if destinations.len() == 0
+    {
+        destinations.push(*range);
+    }
+
+    return destinations;
 }
 
 #[cfg(test)]
